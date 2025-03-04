26ea1455ef64193cb3facba2abea4ee01ed465cd
aloy33 <fsrshad.bahari.33@gmail.com>
2025-03-04 19:50:59 +0330

    add vercel setting

--- server/server.js
+++ server/server.js
@@ -46,7 +46,14 @@ const PORT = process.env.PORT;
 
 mongoose
   .connect(DATABASE_URL)
-  .then((data) => {
-    app.listen(PORT);
-  })
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+
+module.exports = app;
+
+// mongoose
+//   .connect(DATABASE_URL)
+//   .then((data) => {
+//     app.listen(PORT);
+//   })
+//   .catch((err) => console.log(err));
